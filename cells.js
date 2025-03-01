d5ec813196c16bdda8a170f29334519b519076ae
CasualProgrammer25 <86805901+CasualProgrammer25@users.noreply.github.com>
2025-03-01 15:58:06 -0600

    added js file and made small changes

---
+++ cells.js
@@ -0,0 +1,10 @@
+const rows = 10;
+const cols = 10;
+const CellsArray = [];
+
+for (let i = 0; i < rows; i++) {
+  CellsArray[i] = [];
+  for (let j = 0; j < cols; j++) {
+    CellsArray[i][j] = 0;
+  }
+}
