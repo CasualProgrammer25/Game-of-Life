642572f25c7aaf24c93b1b2a628aea0b3abb01cc
Nilendu Bhattacharya <nb0627@Nilendus-MacBook-Pro.local>
2025-03-03 20:51:56 -0600

    fix the css and html for showing table

--- cells.js
+++ cells.js
@@ -1,10 +1,31 @@
-const rows = 10;
-const cols = 10;
-const CellsArray = [];
+function createTable() {
+    const rows = 10;
+    const cols = 10;
+    const CellsArray = [];
 
-for (let i = 0; i < rows; i++) {
-  CellsArray[i] = [];
-  for (let j = 0; j < cols; j++) {
-    CellsArray[i][j] = 0;
-  }
+    var table = document.getElementById('my-table');
+    for (let i = 0; i < rows; i++) {
+      var row = document.createElement('tr');
+      row.className = "aRow";
+
+      CellsArray[i] = [];
+
+      for (let j = 0; j < cols; j++) {
+        CellsArray[i][j] = 0;
+         var td = document.createElement('td');
+         tdContent= document.createElement('div');
+         tdContent.id = "circle";
+         td.appendChild(tdContent);
+         row.appendChild(td)
+      }
+      table.appendChild(row)
+    }
 }
+
+var div = document.getElementById('content');
+var p = document.createElement('p');
+p.style.color = 'black';
+p.textContent = 'I was appended to the div';
+div.appendChild(p);
+
+createTable();
